c7138642665ca292390724008196ff7cc25b1cd4
adnanmahmutovic <mahmutovic2025@gmail.com>
2025-03-02 22:52:19 -0600

    Updated styles added our story

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+function showLogin() {
+    document.getElementById("login-form").style.display = "block";
+    document.getElementById("signup-form").style.display = "none";
+    document.getElementById("login-tab").classList.add("active");
+    document.getElementById("signup-tab").classList.remove("active");
+}
+
+function showSignup() {
+    document.getElementById("login-form").style.display = "none";
+    document.getElementById("signup-form").style.display = "block";
+    document.getElementById("signup-tab").classList.add("active");
+    document.getElementById("login-tab").classList.remove("active");
+}
